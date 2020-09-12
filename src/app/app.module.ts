90a4a42dca945b707c0f352a94f08e4fd9a3d133
Cynthia Costa <cyxcosta@gmail.com>
2020-09-12 16:50:06 -0300

    List component and user component initial structure and style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Components
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module'
+import { ListComponent } from './list/list.component';
+import { UserComponent } from './user/user.component';
+
+// Icons
+import { PayComponent } from './icons/pay/pay.component'
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ListComponent,
+    UserComponent,
+    PayComponent
+   ],
   imports: [
     BrowserModule,
     SharedModule
