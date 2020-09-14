fdd0f97208dce79a165b8431d183beb8b0e7274d
Cynthia Costa <cyxcosta@gmail.com>
2020-09-14 17:29:22 -0300

    Form validation and input mask

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms';
+import { NgxCurrencyModule } from "ngx-currency";
 
 // Components
 import { AppComponent } from './app.component'
@@ -37,7 +38,8 @@ import { CardPipe } from './card.pipe';
     BrowserModule,
     HttpClientModule,
     SharedModule,
-    FormsModule
+    FormsModule,
+    NgxCurrencyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-form',
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent {
+
+  currencyMask = {
+    align: "left",
+    allowZero: true,
+    decimal: ",",
+    precision: 2,
+    prefix: "R$ ",
+    thousands: ".",
+    nullable: true
+  }
 
   cards = [
     // valid card
