c4a9ee6d850703f517f8062eb592199bd9da315e
Micaelly Uchoa <micaellyit@hotmail.com>
2020-09-17 12:02:07 -0300

    input mask has been added to the currency field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CurrencyMaskConfig, CurrencyMaskModule, CURRENCY_MASK_CONFIG } from 'ng2-currency-mask';
 import { AppComponent } from './app.component';
 import { ModalPaymentComponent } from 'src/components/modal-payment/modal-payment.component';
 import { ModalPaymentStatusComponent } from 'src/components/modal-payment-status/modal-payment-status.component';
@@ -8,6 +8,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { MatBadgeModule, MatDialogModule } from '@angular/material';
 import { ReactiveFormsModule } from '@angular/forms';
+
+export const CustomCurrencyMaskConfig: CurrencyMaskConfig = {
+    align: "right",
+    allowNegative: true,
+    decimal: ",",
+    precision: 2,
+    prefix: "R$ ",
+    suffix: "",
+    thousands: "."
+};
+ 
 @NgModule({
     declarations: [AppComponent, ModalPaymentComponent, ModalPaymentStatusComponent],
     imports: [
@@ -17,8 +28,9 @@ import { ReactiveFormsModule } from '@angular/forms';
         MatBadgeModule,
         MatDialogModule,
         ReactiveFormsModule,
+        CurrencyMaskModule,
     ],
-    providers: [],
+    providers: [{ provide: CURRENCY_MASK_CONFIG, useValue: CustomCurrencyMaskConfig }],
     bootstrap: [AppComponent],
     entryComponents: [ModalPaymentComponent, ModalPaymentStatusComponent],
 })
