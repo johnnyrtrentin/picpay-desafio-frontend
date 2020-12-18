ceaffc4e54c1dd4c86471a3aea6523c83a35a697
Carlos Junior <carlosjuniorbbs@gmail.com>
2020-12-18 17:50:03 -0300

    Criando serviço de cartão de crédito

---
+++ src/app/core/services/credit-card.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { CreditCard } from '@core/model/credit-card';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CreditCardService {
+
+  constructor() { }
+
+  getCards(): CreditCard[] {
+    return [
+      // valid card
+      {
+        card_number: '1111111111111111',
+        cvv: 789,
+        expiry_date: '01/18',
+      },
+      // invalid card
+      {
+        card_number: '4111111111111234',
+        cvv: 123,
+        expiry_date: '01/20',
+      },
+    ];
+  }
+}
