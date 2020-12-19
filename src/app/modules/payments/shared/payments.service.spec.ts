49bd5127c78c24fc10d53fdbd70664dccf65205b
Carlos Junior <carlosjuniorbbs@gmail.com>
2020-12-19 01:46:42 -0300

    Criando testes

--- src/app/modules/payments/shared/payments.service.spec.ts
+++ src/app/modules/payments/shared/payments.service.spec.ts
@@ -1,10 +1,24 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { MatDialogModule } from '@angular/material';
+import { HttpErrorResponse } from '@angular/common/http';
 
 import { PaymentsService } from './payments.service';
+import { PaymentTransaction } from './payment-transaction';
 
 describe('PaymentsService', () => {
+
+  let service: PaymentsService;
+  let httpTestingController: HttpTestingController;
+  const BASE_API_URL = 'https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989';
+  const paymentTransaction: PaymentTransaction = {
+    card_number: 'string',
+    cvv: 1,
+    expiry_date: 'string',
+    destination_user_id: 1,
+    value: 1
+  };
+
   beforeEach(() => TestBed.configureTestingModule({
     imports: [
       HttpClientTestingModule,
@@ -12,8 +26,40 @@ describe('PaymentsService', () => {
     ]
   }));
 
+  beforeEach(() => {
+    service = TestBed.get(PaymentsService);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
   it('should be created', () => {
-    const service: PaymentsService = TestBed.get(PaymentsService);
     expect(service).toBeTruthy();
+  });
+
+  it('should make payment', (done) => {
+    service.pay(paymentTransaction).subscribe(data => {
+      done();
+    });
+
+    const req = httpTestingController.expectOne(BASE_API_URL);
+    expect(req.request.method).toBe('POST');
+    req.flush({});
+  });
+
+  it('should return error', (done) => {
+    service.pay(paymentTransaction).subscribe(
+      () => fail('Should have failed'),
+      (error: HttpErrorResponse) => {
+        expect(error.status).toEqual(400);
+        expect(error.error).toContain('400 error');
+        done();
+      }
+    );
+
+    const req = httpTestingController.expectOne(BASE_API_URL);
+    req.flush('400 error', { status: 400, statusText: 'Error 400' });
   });
 });
--- src/app/modules/payments/user-list/user-list.component.spec.ts
+++ src/app/modules/payments/user-list/user-list.component.spec.ts
@@ -1,12 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of, throwError } from 'rxjs';
 
 import { UserListComponent } from './user-list.component';
 import { SharedModule } from '@shared/shared.module';
+import { UserService } from '@core/services/user.service';
+import { User } from '@core/model/user';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
+  let userService: UserService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -24,10 +28,51 @@ describe('UserListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
+    userService = TestBed.get(UserService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should return users', () => {
+    const users: User[] = [{
+      id: 1,
+      name: 'string',
+      img: 'string',
+      username: 'string'
+    }];
+    
+    const spyUserService = spyOn(userService, 'getUsers').and.returnValue(of(users));
+
+    component.onLoadUsers();
+
+    expect(spyUserService).toHaveBeenCalled();
+    expect(component.users).toEqual(users);
+    expect(component.loading).toEqual(false);
+  });
+
+  it('should return error', () => {
+    const spyUserService = spyOn(userService, 'getUsers').and.returnValue(throwError({message: 'error'}));
+
+    component.onLoadUsers();
+
+    expect(spyUserService).toHaveBeenCalled();
+    expect(component.loading).toEqual(false);
+  });
+
+  it('should select user', () => {
+    const user: User = {
+      id: 1,
+      name: 'string',
+      img: 'string',
+      username: 'string'
+    };
+    const spySelect = spyOn(component.select, 'emit');
+
+    component.selectUser(user);
+
+    expect(spySelect).toHaveBeenCalledWith(user);
+});
 });
